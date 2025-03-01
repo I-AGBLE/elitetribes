f6f2c1b4a161e986f51e0cf2a2deff627bba8065
95-Blacksheep <95blacksheep04@gmail.com>
2025-03-01 21:43:02 +0000

    Title text animation

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Title Bar Tex Animation
+const defaultTitleText = "From Void To Signals ...";
+const animatedText = "Be Heard!";
+let isAnimating = true;
+
+function variantTitleText() {
+  if (isAnimating) {
+    document.title = animatedText;
+  } else {
+    document.title = defaultTitleText;
+  }
+  isAnimating = !isAnimating;
+}
+setInterval(variantTitleText, 6000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
