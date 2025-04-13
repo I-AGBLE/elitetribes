846aacbe7dc0aa57f20aa3df91bc526da09187bf
95-Blacksheep <95blacksheep04@gmail.com>
2025-04-13 15:46:29 +0000

    created the floating input icon funtionality and styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    const openFloatingInputBtn = document.querySelector('.open_floating_input');
+    const closeFloatingInputBtn = document.querySelector('.close_floating_input');
+    const postInput = document.querySelector('.floating_post_input');
+    const mainContainer = document.querySelector('main');
+
+    openFloatingInputBtn.addEventListener('click', function () {
+      postInput.style.display = 'block';
+      openFloatingInputBtn.style.display = 'none';
+      closeFloatingInputBtn.style.display = 'block';
+      mainContainer.style.opacity = '0.3'; // You can adjust this value if needed
+    });
+
+    closeFloatingInputBtn.addEventListener('click', function () {
+      postInput.style.display = 'none';
+      openFloatingInputBtn.style.display = 'block';
+      closeFloatingInputBtn.style.display = 'none';
+      mainContainer.style.opacity = '1';
+    });
+  });
+
+
 // Title Bar Text Animation
 const defaultTitleText = "From Void To Signal ...";
 const animatedText = "Be Heard!";
@@ -66,6 +88,8 @@ like_containers.forEach(function(like_container) {
     likeIcon.style.display = 'inline-block';  // Show the like icon
   });
 });
+
+
 
 
 
