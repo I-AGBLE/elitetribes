ea5360ec31cc09fd78fd508eef5b941da7aab834
Isak AGBLE <i.am.agble@gmail.com>
2025-06-23 16:19:45 +0100

    save point

--- unauthorized.php
+++ unauthorized.php
@@ -16,8 +16,7 @@ include 'partials/header.php';
 <main>
   <center>
     <h1>Blocked!</h1>
-    
-              <li><a href="logout_logic.php" id="logout">Logout</a></li>
+      <li><a href="logout_logic.php" id="logout">Logout</a></li>
   </center>
 </main>
 
